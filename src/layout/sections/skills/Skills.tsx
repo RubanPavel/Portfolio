f78af4aa5391a2dc060b45f9027d71181922998a
Ruban Pavel <ruban.public@gmail.com>
2024-01-29 01:42:36 +0300

    add svg in Skills

--- src/layout/sections/skills/Skills.tsx
+++ src/layout/sections/skills/Skills.tsx
@@ -5,14 +5,18 @@ import {Skill} from "./skill/Skill";
 import {Container} from "components/Container";
 
 const skillsArr = [
-  {title: "JAVASCRIPT", iconId: "JS_Logo", width: '100', height: '100', viewBox: '0 0 120 120', grade: 'grade'},
-  {title: "Typescript", iconId: "TS_Logo", width: '100', height: '100', viewBox: '0 0 120 120', grade: 'grade'},
-  {title: "REDUX", iconId: "Redux_Logo", width: '100', height: '100', viewBox: '0 0 120 120', grade: 'grade'},
-  {title: "Figma", iconId: "Figma_Logo", width: '59', height: '90', viewBox: '0 0 59 90', grade: 'grade'},
-  {title: "JAVASCRIPT", iconId: "JS_Logo", width: '100', height: '100', viewBox: '0 0 120 120', grade: 'grade'},
-  {title: "Typescript", iconId: "TS_Logo", width: '100', height: '100', viewBox: '0 0 120 120', grade: 'grade'},
-  {title: "REDUX", iconId: "Redux_Logo", width: '100', height: '100', viewBox: '0 0 120 120', grade: 'grade'},
-  {title: "Figma", iconId: "Figma_Logo", width: '59', height: '90', viewBox: '0 0 59 90', grade: 'grade'}
+  {title: "Javascript", iconId: "JS_Logo", width: '100', height: '100', viewBox: '-1 -4 120 130', grade: 'grade'},
+  {title: "Typescript", iconId: "TS_Logo", width: '100', height: '100', viewBox: '-1 -4 120 130', grade: 'grade'},
+  {title: "Redux", iconId: "Redux_Logo", width: '100', height: '100', viewBox: '0 0 120 120', grade: 'grade'},
+  {title: "Material UI", iconId: "MUI_Logo", width: '120', height: '100', viewBox: '0 0 300 200', grade: 'grade'},
+  {title: "Figma", iconId: "Figma_Logo", width: '70', height: '100', viewBox: '0 0 59 100', grade: 'grade'},
+  {title: "React", iconId: "React_Logo", width: '100', height: '100', viewBox: '0 0 114 101', grade: 'grade'},
+  {title: "HTML", iconId: "HTML", width: '120', height: '100', viewBox: '0 0 120 120', grade: 'grade'},
+  {title: "CSS", iconId: "CSS", width: '120', height: '100', viewBox: '0 0 120 120', grade: 'grade'},
+  {title: "Git", iconId: "Git", width: '100', height: '100', viewBox: '10 8 80 85', grade: 'grade'},
+  {title: "GitHub", iconId: "GitHub", width: '100', height: '100', viewBox: '0 -5 88 100', grade: 'grade'},
+  {title: "NextJS", iconId: "NextJS", width: '140', height: '100', viewBox: '-30 -50 550 400', grade: 'grade'},
+  {title: "Jira", iconId: "Jira", width: '100', height: '100', viewBox: '-20 -20 300 300', grade: 'grade'}
 ];
 
 export const Skills = () => {
