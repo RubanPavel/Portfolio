573f4dc4e92f78af054ffe0566cabb69f66f14d0
Ruban Pavel <ruban.public@gmail.com>
2024-08-02 18:14:07 +0300

    Main change

--- src/layout/sections/main/Main.tsx
+++ src/layout/sections/main/Main.tsx
@@ -12,49 +12,60 @@ export const Main = () => {
   const {t} = useTranslation();
   return (
     <StyledMain id={'home'}>
-      <Container>
-          <MainWrapper>
-            <Name>{t('nameMane')}</Name>
-            <Title>
-              <Typewriter
-                options={{
-                  strings: [`${t('titleMainF')}<br/>${t('titleMainT')}`],
-                  autoStart: true,
-                  loop: true
-                }}
-              />
+      <Container style={{padding: '0 50px', display: 'flex', flexDirection: 'row', justifyContent: 'space-between'}}>
+        <MainWrapper>
+          <p>Hello</p>
+          <Name>I'm {t('nameMane')}</Name>
+          <Title>
+            <Typewriter
+              options={{
+                strings: [`${t('titleMainF')}<br/>${t('titleMainT')}`],
+                autoStart: true,
+                loop: true
+              }}
+            />
+          </Title>
 
-            </Title>
-            <LanguageSwitch/>
-          </MainWrapper>
-        <Photo src={imgMy} alt=""/>
+        </MainWrapper>
+        <div style={{
+          display: 'flex',
+          flexDirection: 'column',
+          justifyContent: 'space-between',
+          alignItems: 'flex-end',
+          width: '45%'
+        }}>
+          <Photo src={imgMy} alt=""/>
+          <LanguageSwitch/>
+        </div>
+
       </Container>
     </StyledMain>
   );
 };
 
 const StyledMain = styled.section`
-  min-height: 100vh;
-  display: flex;
-  align-items: center;
-  width: 100%;
+    min-height: 100vh;
+    display: flex;
+    align-items: center;
+    width: 100%;
 `
 
 const MainWrapper = styled.div`
-  display: flex;
-  margin: 0 0 20px 0;
-  flex-direction: row;
-  justify-content: space-between;
-  align-items: center;
-  flex-wrap: wrap;
-  
-  @media ${Theme.media.tablet}{
-    text-align: center;
     display: flex;
+    margin: 60px 0 20px 0;
+ 
     flex-direction: column;
-    align-items: center;
-    justify-content: center;
-  }
+    /*justify-content: space-around;*/
+    align-items: flex-start;
+    flex-wrap: wrap;
+
+    @media ${Theme.media.tablet} {
+        text-align: center;
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        justify-content: center;
+    }
 `
 
 /*const MainBox = styled.div`
@@ -63,27 +74,27 @@ const MainWrapper = styled.div`
 `*/
 
 const Photo = styled.img`
-  width: 100%;
-  height: 400px;
-  object-fit: cover;
+    width: 100%;
+    height: 400px;
+    object-fit: cover;
+    border-radius: 25px;
 `
 const Name = styled.h2`
-  width: 160px;
-  font-size: 48px;
-  font-weight: 500;
+    font-size: 48px;
+    font-weight: 500;
 
-  @media ${Theme.media.tablet}{
-    width: auto;
-  }
+    @media ${Theme.media.tablet} {
+        width: auto;
+    }
 `
 const Title = styled.h1`
-  
-  padding-top: 56px;
-  font-size: 18px;
-  font-weight: 400;
-  
-  @media ${Theme.media.tablet}{
-    padding-top: 5px;
-  }
-  
+    height: 50px;
+    padding-top: 10px;
+    font-size: 18px;
+    font-weight: 400;
+
+    @media ${Theme.media.tablet} {
+        padding-top: 5px;
+    }
+
 `
